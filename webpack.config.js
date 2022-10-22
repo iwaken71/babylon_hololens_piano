8afcc4e35f5e0025670cb631491ad849c36da470
koukemo <java.sparrow22.surface@gmail.com>
2022-10-22 16:39:02 +0900

    [feat] WebXRのローカル利用に伴い、http➙httpsに変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,35 @@
 const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
-module.exports = {
+const devcert = require("devcert");
+module.exports = async () => {
     // モード値を production に設定すると最適化された状態で、
     // development に設定するとソースマップ有効でJSファイルが出力される
-    mode: "development",
+    const { key, cert } = await devcert.certificateFor('localhost')
+    return {
+        mode: "development",
+        entry: '/src/index.js',
 
-    // ローカル開発用環境を立ち上げる
-    // open:ture 実行時にブラウザが自動的に localhost を開く
-    // webpack-dev-serverのv4の書き方 contentBaseオプションの代わりにstatic以下に書く。
-    
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "dist"),
+        // ローカル開発用環境を立ち上げる
+        // open:ture 実行時にブラウザが自動的に localhost を開く
+        // webpack-dev-serverのv4の書き方 contentBaseオプションの代わりにstatic以下に書く。
+        
+        devServer: {
+            static: {
+                directory: path.join(__dirname, "dist"),
+            },
+            https: {
+                key,
+                cert,
+            },
+            open: true
         },
-        open: true
-    },
 
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { from: "public", to: "." },
-            ],
-        }),
-    ],
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    { from: "public", to: "." },
+                ],
+            }),
+        ],
+    }
 };
